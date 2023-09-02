a5d6ce51647396a1baa4dc783e4ee8c5443dd7c7
José Eduardo Almeida <web2solucoes@gmail.com>
2023-09-02 13:59:49 -0300

    refactor(main.ts): cleaner code in the application boostraper

--- src/main.ts
+++ src/main.ts
@@ -4,31 +4,22 @@ import { idx } from '@/database/IDX';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import JsonViewer from 'vue-json-viewer';
-
 import 'highlight.js/styles/stackoverflow-light.css'
 import hljs from 'highlight.js/lib/core';
 import yaml from 'highlight.js/lib/languages/yaml';
 import typescript from 'highlight.js/lib/languages/typescript';
 import hljsVuePlugin from '@highlightjs/vue-plugin';
+import App from './App.vue';
+import router from './router';
 
 hljs.registerLanguage('yaml', yaml);
 hljs.registerLanguage('typescript', typescript);
-// 
-
-// Import JsonViewer as a Vue.js plugin
-
-import App from './App.vue';
-import router from './router';
 
 idx.start();
 
 const app = createApp(App);
-
 app.use(createPinia());
 app.use(router);
 app.use(JsonViewer);
 app.use(hljsVuePlugin);
-
-
-
 app.mount('#app');
