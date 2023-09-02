db6d5feffa880f193df76c0f33ec35607b06be0e
José Eduardo Almeida <web2solucoes@gmail.com>
2023-09-02 13:39:03 -0300

    feat: oas document validation

--- src/components/OAS.ts
+++ src/components/OAS.ts
@@ -1,3 +1,49 @@
+import { OpenAPIV3 } from "openapi-types";
+import Ajv from "ajv"
+import type { JSONSchemaType } from "ajv"
+
+const ajv = new Ajv();
+
+
+const infoSchema: JSONSchemaType<OpenAPIV3.InfoObject> = {
+  type: "object",
+  properties: {
+    title: {type: "string"},
+    version: {type: "string"},
+    description: {type: "string", nullable: true},
+    termsOfService: {type: "string", nullable: true},
+    contact: {type: "object", required: [], nullable: true},
+    license: {type: "object", required: [], nullable: true},
+  },
+  required: ["title", "version"],
+  additionalProperties: false
+}
+
+const tagSchema: JSONSchemaType<OpenAPIV3.TagObject> = {
+  type: "object",
+  properties: {
+    name: {type: "string"},
+    description: {type: "string", nullable: true},
+    externalDocs: {
+      type: "object",
+      properties: {
+        description: {type: "string", nullable: true},
+        url: {type: "string"},
+      },
+      required: ["url"], 
+      nullable: true
+    },
+  },
+  required: ["name"],
+  additionalProperties: false
+}
+
+
+// validate is a type guard for MyData - type is inferred from schema type
+export const validateAPIInfo = ajv.compile(infoSchema);
+export const validateAPITag = ajv.compile(tagSchema);
+
+
 export const OASDataTypes: string[] = [
     'string',
     'number',
@@ -28,4 +74,8 @@ export const OASFormats: Record<string, string[]> = {
       'email',
       'uuid',
     ]
-  }
+  };
+
+  export type IVersionBranches = Record<string, OpenAPIV3.Document>;
+
+  OpenAPIV3
